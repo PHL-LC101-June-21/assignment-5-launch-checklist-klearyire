6feddc5278d9d84ec3f80b81653c7824e86cb1e6
Kiaran <klearyire@gmail.com>
2021-08-26 18:17:44 -0400

    s

--- script.js
+++ script.js
@@ -38,30 +38,15 @@ window.addEventListener("load", function() {
         event.preventDefault();
    })
 
-   let listedPlanets;
-   // Set listedPlanetsResponse equal to the value returned by calling myFetch()
-   let listedPlanetsResponse = myFetch();
-   listedPlanetsResponse.then(function (result) {
+    listedPlanets;
+    let listedPlanetsResponse = myFetch();
+    listedPlanetsResponse.then(function (result) {
         listedPlanets = result;
         console.log(listedPlanets);
-   }).then(function() {
+    }).then(function() {
         console.log(listedPlanets);
         let pickedPlanet = pickPlanet(listedPlanets);
-        //  Below this comment call the appropriate helper functions to pick a planet fom the list of planets and add that information to your destination.
         const planet = pickPlanet(listedPlanets);
         addDestinationInfo(window.document, pickedPlanet.name, pickedPlanet.diameter, pickedPlanet.star, pickedPlanet.distance, pickedPlanet.moons, pickedPlanet.image);
     })
-
-
-
-        // Nhu's suggestions:
-        // get the form
-        // add a listener to when the form submit
-        // if it's not, preventDefault
-        // get the value for each of the input field
-        // let pilotInput = this.document.querySelector("input[name=pilotName]");
-        // const pilotValue = pilotInput.value
-        // let list = document.getElementById('faultyItems');
-        // formSubmission(document, list, pilotValue, copilotValue, fuelLevelValue, cargoLevelValue)
-
 });
